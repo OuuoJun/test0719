e04e49da24e6913337ac42488d839e80d108bbf8
OuuoJun <2276181401@qq.com>
2019-07-19 10:09:02 +0800

    add HHH

--- src/main/java/com/example/test0719/Test0719Application.java
+++ src/main/java/com/example/test0719/Test0719Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test0719Application {
-
+    //HHH
     public static void main(String[] args) {
         SpringApplication.run(Test0719Application.class, args);
     }
